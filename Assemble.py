518645e0e0cfbba4b9c9e4fa96591c001eff4845
jacklammingdavis-blip <jacklammingdavis@gmail.com>
2026-05-14 09:05:30 -0400

    Update Assemble.py

--- Assemble.py
+++ Assemble.py
@@ -3,7 +3,7 @@ import os
 OPCODES = {
     "ADD": "0000", "SUB": "0001", "XOR": "0010", "XNOR": "0011",
     "OR":  "0100", "AND": "0101", "NOR": "0110", "NAND": "0111",
-    "LDI": "1000"
+    "LDI": "1000", "HLT":  "1001"
 }
 
 REGISTERS = {f"R{i}": format(i, "04b") for i in range(1, 16)}
